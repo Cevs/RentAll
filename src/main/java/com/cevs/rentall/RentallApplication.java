5ad4dc7d89ee29e0f231064238b2b3967a97b84f
Alen Martinčević <martincevic.alen.development@gmail.com>
2018-06-27 11:18:03 +0200

    Project initialization

---
+++ src/main/java/com/cevs/rentall/RentallApplication.java
@@ -0,0 +1,12 @@
+package com.cevs.rentall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentallApplication.class, args);
+	}
+}
